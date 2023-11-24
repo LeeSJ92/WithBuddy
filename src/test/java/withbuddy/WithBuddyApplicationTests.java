51627db003e4ac7fc9af057935172e1612558b0f
devchyun96 <devchyun96@naver.com>
2023-11-24 15:34:10 +0900

    Initial Commit

---
+++ src/test/java/withbuddy/WithBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package withbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
